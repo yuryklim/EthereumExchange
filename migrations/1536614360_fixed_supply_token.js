142fdee686241bff80419d670a43c453341392ec
yuryklim <yuryklim@outlook.com>
2018-09-13 19:26:25 +0300

    add Exchange.sol

--- migrations/1536614360_fixed_supply_token.js
+++ migrations/1536614360_fixed_supply_token.js
@@ -1,9 +1,9 @@
 var FixedSupplyToken = artifacts.require("./FixedSupplyToken.sol");
 var Owned = artifacts.require("./Owned.sol");
-
+var Exchange = artifacts.require("./Exchange.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Owned);
+  deployer.deploy(Exchange);
   deployer.deploy(FixedSupplyToken);
-
 };
