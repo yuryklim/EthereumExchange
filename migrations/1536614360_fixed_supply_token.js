3f8735d7be87cb46bbaed3fa03a1699c41892c8a
yuryklim <yuryklim@outlook.com>
2018-09-13 19:33:01 +0300

    add library SafeMath.sol

--- migrations/1536614360_fixed_supply_token.js
+++ migrations/1536614360_fixed_supply_token.js
@@ -1,9 +1,11 @@
 var FixedSupplyToken = artifacts.require("./FixedSupplyToken.sol");
 var Owned = artifacts.require("./Owned.sol");
+var SafeMath = artifacts.require("./SafeMath.sol");
 var Exchange = artifacts.require("./Exchange.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Owned);
+  deployer.deploy(SafeMath);
   deployer.deploy(Exchange);
   deployer.deploy(FixedSupplyToken);
 };
